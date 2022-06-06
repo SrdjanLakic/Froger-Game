efd4d1d201450c7193479f3b7077565f4fdcc0ac
SrdjanLakic <srdjan949@gmail.com>
2022-06-06 16:34:47 -0400

    moveLogLeft method added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const timeLeftDisplay = document.querySelector('#time-left');
 const resultDisplay = document.querySelector('#result');
 const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
+const logsLeft = document.querySelectorAll('.log-left');
+
 let currentIndex = 76;
 const width = 9;
 
@@ -28,3 +30,34 @@ function moveFrog(e) {
 }
 
 document.addEventListener('keyup', moveFrog);
+
+function autoMoveLogs() {
+  logsLeft.forEach((logLeft) => moveLogLeft(logLeft));
+}
+
+function moveLogLeft(logLeft) {
+  switch (true) {
+    case logLeft.classList.contains('l1'):
+      logLeft.classList.remove('l1');
+      logLeft.classList.add('l2');
+      break;
+    case logLeft.classList.contains('l2'):
+      logLeft.classList.remove('l2');
+      logLeft.classList.add('l3');
+      break;
+    case logLeft.classList.contains('l3'):
+      logLeft.classList.remove('l3');
+      logLeft.classList.add('l4');
+      break;
+    case logLeft.classList.contains('l4'):
+      logLeft.classList.remove('l4');
+      logLeft.classList.add('l5');
+      break;
+    case logLeft.classList.contains('l5'):
+      logLeft.classList.remove('l5');
+      logLeft.classList.add('l1');
+      break;
+  }
+}
+
+setInterval(autoMoveLogs, 1000);
